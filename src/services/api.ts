ef5bf0285dd594d3c657ab7d5332ccd365fd7f49
muhammaaddsafii-dev <muhammaaddsafii@gmail.com>
2026-01-31 13:19:52 +0700

    change BE-url for API

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { KegiatanAPI, FotoKegiatan, JenisKegiatan, StatusKegiatan } from '@/types';
 
 // Base URL untuk API - sesuaikan dengan backend Anda
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://be-dhaharan-278881327745.asia-southeast1.run.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
